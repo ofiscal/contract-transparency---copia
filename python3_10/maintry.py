75aed9d23d7174b6f9af9d942ad7bcd17d096fcc
Daniel-Duque <dduque80@hotmail.com>
2023-01-25 17:44:49 -0500

    started transformers work

---
+++ python3_10/maintry.py
@@ -0,0 +1,9 @@
+#load the dataset using cleaning
+
+#train the models or update them
+
+#run the models with the dataset
+
+#send the updated excel file to process via tableau
+
+#
---
+++ python3_10/modules/models/Transformers.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 25 16:09:56 2023
+
+@author: Daniel Duque
+"""
