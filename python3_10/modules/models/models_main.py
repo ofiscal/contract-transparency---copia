210e433a8b250b4e98a7ad9a4cf2eb11892d91ba
Daniel-Duque <dduque80@hotmail.com>
2023-02-01 17:43:13 -0500

    creation of loading method and adding method

--- python3_10/modules/models/models_main.py
+++ python3_10/modules/models/models_main.py
@@ -7,11 +7,37 @@ Created on Wed Feb  1 15:18:20 2023
 
 import tensorflow as tf
 from tensorflow import keras
+import python_3_10.modules.models_main.Transformers as Transformers
+import pandas as pd
 
 class joining():
     def __init__(self):
         ...
     
-    def load_models(path):
-        self.Transformers=
+    def load_models(self,path_to_models):
+        self.Transformer=Transformers.create_model_tr()
+        self.Transformer.load_weights(path_to_models+"\model1_tr.hdf5")
+        
+    
+    def train_model (self,
+        model:str,#Transformer, RNN, or...
+        epocas:float,
+        X: pd.Series,
+        Y:pd.Series,
+        path_to_models
+        ):
+        
+        if model=="Transformer":
+            self.Transformer=Transformers.keep_train(self.Transformer,
+                        epocas=10,
+                        X=X,
+                        Y=Y,
+                        checkpointpath=path_to_models+"\model1_tr.hdf5")
+        
+    
+    def __add__(self,
+        models=list
+                ):
+        ...
+    
         
