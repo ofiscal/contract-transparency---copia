5893c3ac72fb2f3f2bc7c191980b2ced9cd51a79
Daniel-Duque <dduque80@hotmail.com>
2025-04-15 20:27:04 -0500

    not working most certainly

--- python3_10/modules/models/main_single_row_mbert_neural.py
+++ python3_10/modules/models/main_single_row_mbert_neural.py
@@ -7,30 +7,48 @@ Original file is located at
 """
 ###
 ###
+import pandas as pd
+import datetime as dt
+import numpy as np
+import os
+from sentence_transformers import util
+from sentence_transformers import SentenceTransformer
+import sklearn
+import pickle
+
+
+def change_dolar(local_value:float,
+    change_rate:float):
+  return local_value*change_rate
+
+def semantic_search(prompt, df, model, top_k=1000):
+    # Generate embeddings for the descriptions
+    df['embeddings'] = df['Descripcion del Proceso'].apply(lambda x: model.encode(x, convert_to_tensor=True))
+
+    prompt_embedding = model.encode(prompt, convert_to_tensor=True)
+    similarities = df['embeddings'].apply(lambda x: util.pytorch_cos_sim(prompt_embedding, x).item())
+    df['similarity'] = similarities
+    return df
+def switch_time(frecue,dato):
+    if frecue=="Dia(s)":
+        return dato
+    elif frecue=="Mes(es)":
+        return dato*30
+    elif frecue=="Año(s)":
+        return dato*365
+    elif frecue=="Hora(s)":
+        return dato/24
+    elif frecue=="Semana(s)":
+        return dato*168
+    else:
+        return dato
+
+
 for numerator in range(0,100):
-    import pandas as pd
-    import datetime as dt
-    import numpy as np
-    import os
-    from sentence_transformers import util
-    from sentence_transformers import SentenceTransformer
-    import sklearn
+
     prompt = "Pauta | Publicidad | Prensa | Periodismo | Periodista | Divulgación | Multimedia publicitaria | Redes Sociales | propaganda en Televisión | publicidad en Radio | cuña Radial | Periódico |propaganda Audiovisual | Video | Revista | Comunicaciones divulgativas"
 
-    def change_dolar(local_value:float,
-        change_rate:float):
-      return local_value*change_rate
-    
-    def semantic_search(prompt, df, model, top_k=1000):
-        # Generate embeddings for the descriptions
-        df['embeddings'] = df['Descripcion del Proceso'].apply(lambda x: model.encode(x, convert_to_tensor=True))
 
-        prompt_embedding = model.encode(prompt, convert_to_tensor=True)
-        similarities = df['embeddings'].apply(lambda x: util.pytorch_cos_sim(prompt_embedding, x).item())
-        df['similarity'] = similarities
-        return df
-    
-    
     path_change=r"data/sucio/Datos históricos USD_COP.xlsx"
     path_data=r"/content/drive/MyDrive/Hack Corruption - Contractor/datos/records.csv"
     path_CPI=r"data/sucio/USA_CPI.xlsx"
@@ -83,6 +101,9 @@ for numerator in range(0,100):
     model = SentenceTransformer("tomaarsen/static-similarity-mrl-multilingual-v1")
     records['duracion numero'] = records["Duración del contrato"].str.extract(r'(\d+[.\d]*)').astype(float).replace(np.nan,0)
     records['duracion valor'] = records["Duración del contrato"].str.replace(r'(\d+[.\d]*)','').replace(np.nan,"0")
+    records["frecuenc"]=records['duracion valor'].apply(lambda x:x.split(" ")[-1])
+    
+    records["dias"]=records.apply(lambda row:switch_time(frecue=row["frecuenc"],dato=row['duracion numero']),axis=1)
     records=records.dropna(subset=["Descripcion del Proceso","value_thousand_dolar"])
     
     #records=semantic_search(prompt,records,model)
@@ -96,7 +117,7 @@ for numerator in range(0,100):
                    
                    'Tipo de Contrato',
                    "Codigo de Categoria Principal",
-                   'duracion valor'
+
 
                    ]
     #genera una reducción de duplicados para evitar problema de nuevas variables en estimación
@@ -117,18 +138,25 @@ for numerator in range(0,100):
     
     numpdata=joinedpar['Descripcion del Proceso'].apply(lambda x: model.encode(x, convert_to_numpy=True))
     embeddings=pd.DataFrame(np.array([np.array(xi) for xi in numpdata]))
-
+    joined = categ.merge(embeddings,left_index=True, right_index=True)
+    X=joined
+    X.columns = X.columns.astype(str) 
     #variables_reg=["compiledRelease/tender/enquiryPeriod/durationInDays"]
 
     neuralmodel= sklearn.neural_network.MLPRegressor(hidden_layer_sizes=(50,10,),
         activation='relu', solver='adam', alpha=0.01, batch_size='auto',
         learning_rate='adaptive', validation_fraction=0.1,random_state=0,
         verbose=True, early_stopping=True,warm_start=True)
+    if False:
+        neuralmodel.fit(X,records["value_thousand_dolar"])
+        pickle.dump(neuralmodel, open(r'model_saved_torch\modelneuralmber.pkl','wb'))
+    else:
+        with open(r'model_saved_torch\modelneuralmber.pkl', "rb") as input_file:
+          neuralmodel = pickle.load(input_file)
+          
+    X[[x for x in X.columns if x not in neuralmodel.feature_names_in_.tolist()]] = 0
     
-    joined = categ.merge(embeddings,left_index=True, right_index=True)
-    X=joined
-    X.columns = X.columns.astype(str) 
-    neuralmodel.fit(X,records["value_thousand_dolar"])
+    
     hat=neuralmodel.predict(X)
     
     neuralmodel.score(X,records["value_thousand_dolar"])
